a116ed8f15f4d53db007bd4d8bf47c47d6d59616
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-18 18:25:18 +0000

    Use click for CLI, and show better count after execution.

--- flake8_rst_docstrings_sphinx/__main__.py
+++ flake8_rst_docstrings_sphinx/__main__.py
@@ -22,24 +22,118 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #  THE SOFTWARE.
 #
+#  "Application" based on Flake8
+#  Copyright (C) 2011-2013 Tarek Ziade <tarek@ziade.org>
+#  Copyright (C) 2012-2016 Ian Cordasco <graffatcolmingov@gmail.com>
+#  MIT Licensed
+#
 
 # stdlib
 import re
 import sys
-from argparse import ArgumentParser
 from configparser import ConfigParser
-from typing import List, Optional
+from functools import partial
+from gettext import ngettext
+from typing import List, Optional, Tuple
 
 # 3rd party
-import flake8.main.cli  # type: ignore
+import click
+import flake8.main.application  # type: ignore
+from click import Context
+from consolekit import CONTEXT_SETTINGS
+from consolekit.options import MultiValueOption
 
 # this package
 from flake8_rst_docstrings_sphinx import AutodocFormatter, Formatter, ToolboxFormatter
 
 __all__ = ["main"]
 
+_error = partial(ngettext, "error", "errors")
+_file = partial(ngettext, "file", "files")
 
-def main(argv: Optional[List[str]] = None) -> None:
+
+class Application(flake8.main.application.Application):
+	"""
+	Subclass of Flake8's ``Application``.
+	"""
+
+	def exit(self) -> None:
+		"""
+		Handle finalization and exiting the program.
+
+		This should be the last thing called on the application instance.
+		It will check certain options and exit appropriately.
+		"""
+
+		if self.options.count:
+			files_checked = self.file_checker_manager.statistics["files"]
+			files_with_errors = self.file_checker_manager.statistics["files_with_errors"]
+			if self.result_count:
+				click.echo(
+						f"Found {self.result_count} {_error(self.result_count)} "
+						f"in {files_with_errors} {_file(files_with_errors)} "
+						f"(checked {files_checked} source {_file(files_checked)})"
+						)
+			else:
+				click.echo(f"Success: no issues found in {files_checked} source {_file(files_checked)}")
+
+		if self.options.exit_zero:
+			raise SystemExit(self.catastrophic_failure)
+		else:
+			raise SystemExit((self.result_count > 0) or self.catastrophic_failure)
+
+	def report_errors(self) -> None:
+		"""
+		Report all the errors found by flake8 3.0.
+
+		This also updates the :attr:`result_count` attribute with the total
+		number of errors, warnings, and other messages found.
+		"""
+
+		flake8.main.application.LOG.info("Reporting errors")
+
+		files_with_errors = results_reported = results_found = 0
+
+		for checker in self.file_checker_manager._all_checkers:
+			results_ = sorted(checker.results, key=lambda tup: (tup[1], tup[2]))
+			filename = checker.display_name
+
+			with self.file_checker_manager.style_guide.processing_file(filename):
+				results_reported_for_file = self.file_checker_manager._handle_results(filename, results_)
+				if results_reported_for_file:
+					results_reported += results_reported_for_file
+					files_with_errors += 1
+
+			results_found += len(results_)
+
+		results: Tuple[int, int] = (results_found, results_reported)
+
+		self.total_result_count, self.result_count = results
+		flake8.main.application.LOG.info(
+				"Found a total of %d violations and reported %d",
+				self.total_result_count,
+				self.result_count,
+				)
+
+		self.file_checker_manager.statistics["files_with_errors"] = files_with_errors
+
+
+def compile_options(
+		rst_roles: Optional[List[str]],
+		rst_directives: Optional[List[str]],
+		*,
+		allow_autodoc: bool = False,
+		allow_toolbox: bool = False,
+		):
+	"""
+	Compile the list of allowed roles and directives.
+
+	:param rst_roles:
+	:param rst_directives:
+	:param allow_autodoc:
+	:param allow_toolbox:
+	"""
+
 	default_allowed_rst_directives = []
 	default_allowed_rst_roles = []
 
@@ -52,44 +146,88 @@ def main(argv: Optional[List[str]] = None) -> None:
 		if "rst-roles" in config["flake8"]:
 			default_allowed_rst_roles.extend(re.split(r"[\n,]", config["flake8"]["rst-roles"]))
 
-	parser = ArgumentParser()
-	parser.add_argument("--rst-directives", type=list, default=None)  # type: ignore
-	parser.add_argument("--rst-roles", type=list, default=None)  # type: ignore
-	parser.add_argument("--disallow-sphinx", action="store_true", default=False)
-	parser.add_argument("--allow-autodoc", action="store_true", default=False)
-	parser.add_argument("--allow-toolbox", action="store_true", default=False)  # implies allow-autodoc
-
-	if argv is None:
-		argv = sys.argv[1:]
-
-	args, unknown = parser.parse_known_args(argv)
-
-	if args.rst_roles is None:
-		if args.allow_toolbox:
-			args.rst_roles = sorted({*default_allowed_rst_roles, *ToolboxFormatter.allowed_rst_roles})
-		elif args.allow_autodoc:
-			args.rst_roles = sorted({*default_allowed_rst_roles, *AutodocFormatter.allowed_rst_roles})
+	if rst_roles is None:
+		if allow_toolbox:
+			rst_roles = sorted({*default_allowed_rst_roles, *ToolboxFormatter.allowed_rst_roles})
+		elif allow_autodoc:
+			rst_roles = sorted({*default_allowed_rst_roles, *AutodocFormatter.allowed_rst_roles})
 		else:
-			args.rst_roles = sorted({*default_allowed_rst_roles, *Formatter.allowed_rst_roles})
+			rst_roles = sorted({*default_allowed_rst_roles, *Formatter.allowed_rst_roles})
 
-	if args.rst_directives is None:
-		if args.allow_toolbox:
-			args.rst_directives = sorted({
-					*default_allowed_rst_directives, *ToolboxFormatter.allowed_rst_directives
-					})
-		elif args.allow_autodoc:
-			args.rst_directives = sorted({
-					*default_allowed_rst_directives, *AutodocFormatter.allowed_rst_directives
-					})
+	if rst_directives is None:
+		if allow_toolbox:
+			rst_directives = sorted({*default_allowed_rst_directives, *ToolboxFormatter.allowed_rst_directives})
+		elif allow_autodoc:
+			rst_directives = sorted({*default_allowed_rst_directives, *AutodocFormatter.allowed_rst_directives})
 		else:
-			args.rst_directives = sorted({*default_allowed_rst_directives, *Formatter.allowed_rst_directives})
+			rst_directives = sorted({*default_allowed_rst_directives, *Formatter.allowed_rst_directives})
 
-	if not args.disallow_sphinx:
-		unknown.append(f"--rst-directives=" + ','.join(args.rst_directives))
-		unknown.append(f"--rst-roles=" + ','.join(args.rst_roles))
+	return rst_roles, rst_directives
 
-	flake8.main.cli.main(unknown)
+
+@click.option(
+		"--rst-roles",
+		type=click.STRING,
+		default=None,
+		cls=MultiValueOption,
+		help="List of roles to allow instead of the default",
+		)
+@click.option(
+		"--rst-directives",
+		type=click.STRING,
+		default=None,
+		cls=MultiValueOption,
+		help="List of directives to allow instead of the default",
+		)
+@click.option(
+		"--disallow-sphinx",
+		is_flag=True,
+		default=False,
+		help="Don't allow any sphinx roles and directives (function like vanilla Flake8)",
+		)
+@click.option(
+		"--allow-autodoc",
+		is_flag=True,
+		default=False,
+		help="Whether to allow autodoc's roles and directives.",
+		)
+@click.option(
+		"--allow-toolbox",
+		is_flag=True,
+		default=False,
+		help="Whether to allow sphinx-toolbox's roles and directives. Implies '--allow-autodoc'.",
+		)
+@click.command(context_settings={"ignore_unknown_options": True, "allow_extra_args": True, **CONTEXT_SETTINGS})
+@click.pass_context
+def main(
+		ctx: Context,
+		rst_directives: List[str],
+		rst_roles: List[str],
+		disallow_sphinx: bool,
+		allow_autodoc: bool,
+		allow_toolbox: bool,
+		):
+	"""
+	Wrapper around flake8 and flake8-rst-docstrings to filter out warnings related to Sphinx's built in options.
+	"""
+
+	unknown = ctx.args[:]
+
+	rst_roles, rst_directives = compile_options(
+		rst_roles,
+		rst_directives,
+		allow_autodoc=allow_autodoc,
+		allow_toolbox=allow_toolbox,
+		)
+
+	if not disallow_sphinx:
+		unknown.append(f"--rst-directives=" + ','.join(rst_directives))
+		unknown.append(f"--rst-roles=" + ','.join(rst_roles))
+
+	app = Application()
+	app.run(unknown)
+	app.exit()
 
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+	sys.exit(main(obj={}))
